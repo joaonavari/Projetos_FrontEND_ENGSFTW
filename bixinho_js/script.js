d0f1734cc21891fc7b7f2ca3aefc4a32df37525d
Joao Vitor Navari <joaovitorne.16@gmail.com>
2026-04-06 00:17:50 -0300

    Atualização de alguns bugs/erros, e adição de botão secrectgo com foto do gostoosoosoinho

--- bixinho_js/script.js
+++ bixinho_js/script.js
@@ -1,5 +1,6 @@
 const bicho = document.getElementById("bicho");
 const btn = document.getElementById("btn");
+const areaSegreta = document.getElementById("segredo-ferlini");
 
 const estados = {
     normal: "gonbicho.png",
@@ -15,37 +16,47 @@ let time_Click = null;
 let time_Out = null;
 
 
-function init_cont (){
-    if(intervalo) clearInterval(intervalo);
+function init_cont() {
+    if (intervalo) clearInterval(intervalo);
 
-    intervalo = setInterval( ()=> {
+    intervalo = setInterval(() => {
         contador++;
         console.log("Tempo:", contador);
 
-        if (contador == 10){
+        if (contador == 10) {
             bicho.src = estados.fome30
         }
-        if (contador == 20){
+        if (contador == 20) {
             bicho.src = estados.fome60
         }
-        
+        if (contador == 20) {
+            bicho.src = estados.fome60;
+            areaSegreta.classList.remove("hidden"); 
+        }
+
     }, 1000);
 }
 
-function alimentar(){
-    bicho.crc = estados.comendo;
-    contador = 0; 
+function alimentar() {
+    bicho.src = estados.clicado;
+    contador = 0;
     console.log("Comendo");
 
     if (time_Click) clearInterval(setTimeout)
 
-        time_Click = setTimeout(()=>{
-            bicho.src = estados.alimentado;
-                
-            time_Out = setTimeout(() => {
-                    bicho.src = estados.normal;
-                }, 2000);
-        }, 1000);
+    time_Click = setTimeout(() => {
+        bicho.src = estados.alimentado;
+
+        time_Out = setTimeout(() => {
+            bicho.src = estados.normal;
+        }, 2000);
+    }, 1000);
+}
+
+function salvacaoFerlini() {
+    alert("O Gostosinho salvou seu pet da fome!");
+    areaSegreta.classList.add("hidden"); 
+    alimentar(); 
 }
 
 init_cont();
